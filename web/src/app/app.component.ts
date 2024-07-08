c34ed30992dcb72a017675e27a100c801341c095
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2024-07-08 21:58:14 +0200

    add a navbar, a basic footer and link the navbar with basis  pages like home, register and login #3

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ApiService } from './service/api.service';
+import { HeaderComponent } from './component/layout/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { FooterComponent } from './component/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
