b12ed287f5eb8ccac30586c347b54f27811c11f1
Insanytil <116179598+Insanytil@users.noreply.github.com>
2024-07-09 23:15:00 +0200

    HTML & SCSS of Login and Register forms #4

--- web/src/app/component/register/register.component.ts
+++ web/src/app/component/register/register.component.ts
@@ -1,21 +1,34 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../service/auth.service';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm!: FormGroup;
+  submitted = false;
   usernames: string[] = [];
   emails: string[] = [];
 
-
-  constructor(private auth: AuthService) {  };
+  constructor(private auth: AuthService, private formBuilder: FormBuilder) {  };
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3), this.noWhitespaceValidator]],
+      email: ['', [Validators.required, Validators.email, this.emailValidator]],
+      password: ['', [Validators.required, Validators.minLength(8), this.passwordValidator]],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validator: this.mustMatch('password', 'confirmPassword')
+    });
+
     this.auth.getUsers().subscribe(
       (data) => {
         this.usernames = data.map((user: any) => user.username);
@@ -23,6 +36,58 @@ export class RegisterComponent {
       },
       (error) => {console.log(error)}
     );
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // Handle valid form submission here
+  }
+
+  noWhitespaceValidator(control: AbstractControl): ValidationErrors | null {
+    if (control.value && control.value.indexOf(' ') >= 0) {
+      return { whitespace: true };
+    }
+    return null;
+  }
+
+  emailValidator(control: AbstractControl): ValidationErrors | null {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (control.value && !emailRegex.test(control.value)) {
+      return { emailInvalid: true };
+    }
+    return null;
+  }
+
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (control.value && !passwordRegex.test(control.value)) {
+      return { passwordInvalid: true };
+    }
+    return null;
+  }
+
+  mustMatch(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passControl = formGroup.controls[password];
+      const confirmPassControl = formGroup.controls[confirmPassword];
+
+      if (confirmPassControl.errors && !confirmPassControl.errors['mustMatch']) {
+        return;
+      }
+
+      if (passControl.value !== confirmPassControl.value) {
+        confirmPassControl.setErrors({ mustMatch: true });
+      } else {
+        confirmPassControl.setErrors(null);
+      }
+    };
   }
 
 }
