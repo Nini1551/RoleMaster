2e884f3c6d654794fa18860cdc7b56c43f9a7f04
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2024-07-09 09:12:00 +0200

    resolve conflict

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
 import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
-import { HomeComponent } from './component/home/home.component';
 
 export const routes: Routes = [
   {
