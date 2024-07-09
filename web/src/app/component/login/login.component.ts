d2c19fc9d0041c4e9368116adcc683332fa6a367
Insanytil <116179598+Insanytil@users.noreply.github.com>
2024-07-09 23:15:00 +0200

    HTML & SCSS of Login and Register forms #4

--- web/src/app/component/login/login.component.ts
+++ web/src/app/component/login/login.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: '',
+      password: ''
+    
+    })
+  }
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    // Handle valid form submission here
+  }
 
 }
